ba9a3744782d389dc3c2016bb1148c896b1c0d9d
michrytel <76482065+michrytel@users.noreply.github.com>
2021-03-19 20:11:47 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title';
 import UploadForm from "./components/UploadForm";
 import Images from "./components/Images";
+import Modal from "./components/Modal";
 
 function App() {
-  return (
+    const [selectedImg, setSelectedImg] = useState(null);
+    return (
     <div className="App">
       <Title/>
       <UploadForm/>
-      <Images/>
+        <Images setSelectedImg={setSelectedImg} />
+        { selectedImg && (
+            <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        )}
     </div>
   );
 }
