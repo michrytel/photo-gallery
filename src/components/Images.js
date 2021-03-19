085f4e52c9d91146cc5a4a81f8b355e6914fa49e
michrytel <76482065+michrytel@users.noreply.github.com>
2021-03-19 18:04:30 +0100

    displaying images

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,10 +1,13 @@
 import React from "react";
-
+import useFirestore from "../hooks/useFirestore";
 const Images = () => {
-
+    const {docs} = useFirestore(`images`)
     return (
         <div className="images">
-            images
+            {docs && docs.map(doc => (
+                <div className="img" key={doc.id}><img src={doc.url} alt="collection"/></div>
+                )
+            )}
         </div>
     )
 }
